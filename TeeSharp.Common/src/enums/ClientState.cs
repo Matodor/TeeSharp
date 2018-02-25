84b89e5987333a966110c57ee1134ad4a70844bf
Matodor <matodor-zlo@yandex.ru>
2018-02-25 15:01:45 +0500

    ClientState

---
+++ TeeSharp.Common/src/enums/ClientState.cs
@@ -0,0 +1,12 @@
+﻿namespace TeeSharp.Common.Enums
+{
+    public enum ClientState
+    {
+        OFFLINE = 0,
+        CONNECTING,
+        LOADING,
+        ONLINE,
+        DEMOPLAYBACK,
+        QUITING,
+    }
+}
