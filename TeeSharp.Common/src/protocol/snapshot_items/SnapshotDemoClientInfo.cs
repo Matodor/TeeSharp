a9827593165d5c9a5ffabce8ca919070546df445
Matodor <matodor-zlo@yandex.ru>
2018-12-22 10:12:09 +0500

    [WIP] NOT COMPILED! SnapshotDemoClientInfo helper properties

--- TeeSharp.Common/src/protocol/snapshot_items/SnapshotDemoClientInfo.cs
+++ TeeSharp.Common/src/protocol/snapshot_items/SnapshotDemoClientInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 using TeeSharp.Common.Enums;
 using TeeSharp.Common.Snapshots;
+using TeeSharp.Core;
 
 namespace TeeSharp.Common.Protocol
 {
@@ -15,6 +16,54 @@ namespace TeeSharp.Common.Protocol
             set => Local = value ? 1 : 0;
         }
 
+        public string Name
+        {
+            get => IntName.IntsToStr();
+            set => IntName = value.StrToInts(4);
+        }
+
+        public string Clan
+        {
+            get => IntClan.IntsToStr();
+            set => IntClan = value.StrToInts(3);
+        }
+
+        public string SkinPartName1
+        {
+            get => IntSkinPartName1.IntsToStr();
+            set => IntSkinPartName1 = value.StrToInts(6);
+        }
+
+        public string SkinPartName2
+        {
+            get => IntSkinPartName2.IntsToStr();
+            set => IntSkinPartName2 = value.StrToInts(6);
+        }
+
+        public string SkinPartName3
+        {
+            get => IntSkinPartName3.IntsToStr();
+            set => IntSkinPartName3 = value.StrToInts(6);
+        }
+
+        public string SkinPartName4
+        {
+            get => IntSkinPartName4.IntsToStr();
+            set => IntSkinPartName4 = value.StrToInts(6);
+        }
+
+        public string SkinPartName5
+        {
+            get => IntSkinPartName5.IntsToStr();
+            set => IntSkinPartName5 = value.StrToInts(6);
+        }
+
+        public string SkinPartName6
+        {
+            get => IntSkinPartName6.IntsToStr();
+            set => IntSkinPartName6 = value.StrToInts(6);
+        }
+
         [MarshalAs(UnmanagedType.I4)] public int Local;
         [MarshalAs(UnmanagedType.I4)] public Team Team;
 
@@ -23,12 +72,12 @@ namespace TeeSharp.Common.Protocol
 
         [MarshalAs(UnmanagedType.I4)] public int Country;
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartNames1;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartNames2;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartNames3;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartNames4;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartNames5;
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartNames6;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] IntSkinPartName1;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] IntSkinPartName2;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] IntSkinPartName3;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] IntSkinPartName4;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] IntSkinPartName5;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] IntSkinPartName6;
 
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] UseCustomColors;
         [MarshalAs(UnmanagedType.ByValArray, SizeConst = 6)] public int[] SkinPartColors;
