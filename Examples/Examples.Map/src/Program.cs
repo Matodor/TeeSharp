2daca5f6ee30bb8fa649c38fe875d3008d3d2ab4
Matodor <matodor-zlo@yandex.ru>
2020-10-04 00:06:44 +0400

    Examples.Map project

---
+++ Examples/Examples.Map/src/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using TeeSharp.Map;
+
+namespace Examples.Map
+{
+    internal static class Program
+    {
+        private const string MapName = "Gold Mine";
+
+        private static void Main(string[] args)
+        {
+            using (var stream = File.OpenRead($"maps/{MapName}.map"))
+            {
+                if (stream == null)
+                {
+                }
+                else
+                {
+                    var dataFile = DataFileReader.Read(stream, out var error);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
