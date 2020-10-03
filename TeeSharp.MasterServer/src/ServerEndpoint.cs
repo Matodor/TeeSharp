5784bf2554b4d0ad32ec6d63e075049e7b24e446
Matodor <matodor-zlo@yandex.ru>
2020-10-04 00:11:38 +0400

    MasterServer ServerEndpoint helper class

--- TeeSharp.MasterServer/src/ServerEndpoint.cs
+++ TeeSharp.MasterServer/src/ServerEndpoint.cs
@@ -1,14 +1,34 @@
+using System;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using TeeSharp.Network;
 
 namespace TeeSharp.MasterServer
 {
-    [StructLayout(LayoutKind.Sequential, Pack = 1)]
-    public struct ServerEndpoint
+    public static class ServerEndpoint
     {
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
-        public byte[] IpData;
+        /**
+         * 16 bytes for IP address, 2 bytes for port
+         */
+        public const int SizeOfAddr = 18;
+        
+        public static IPEndPoint Get(Span<byte> data)
+        {
+            var isIpV4 = NetworkConstants.IpV4Mapping.AsSpan().SequenceEqual(data.Slice(0, 12));
+            var port = (data[16] << 8) | data[17];
 
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
-        public byte[] PortData;
+            return new IPEndPoint(new IPAddress(isIpV4 ? data.Slice(12, 4) : data), port);
+        }
+        
+        public static IPEndPoint[] GetArray(Span<byte> data)
+        {
+            var array = new IPEndPoint[data.Length / SizeOfAddr];
+
+            for (var i = 0; i < array.Length; i++)
+                array[i] = Get(data.Slice(i * SizeOfAddr));
+
+            return array;
+        }
     }
 }
