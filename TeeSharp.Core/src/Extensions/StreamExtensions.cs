7ac0ce35fe124c781a1d443c6545f3efcb255513
Matodor <matodor-zlo@yandex.ru>
2020-10-05 02:24:10 +0400

    StreamExtensions GetStruct/GetStructs methods

--- TeeSharp.Core/src/Extensions/StreamExtensions.cs
+++ TeeSharp.Core/src/Extensions/StreamExtensions.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace TeeSharp.Core.Extensions
 {
@@ -6,7 +8,41 @@ namespace TeeSharp.Core.Extensions
     {
         public static bool GetStruct<T>(this Stream stream, out T output) where T : struct
         {
-            output = new T();
+            var size = TypeHelper<T>.Size;
+            if (stream.Position + size >= stream.Length)
+            {
+                output = default;
+                return false;
+            }
+
+            var buffer = new Span<byte>(new byte[size]);
+            if (buffer.Length != stream.Read(buffer))
+            {
+                output = default;
+                return false;
+            }
+
+            output = buffer.ToStruct<T>();
+            return true;
+        }
+
+        public static bool GetStructs<T>(this Stream stream, int count, out Span<T> output) where T : struct
+        {
+            var size = TypeHelper<T>.Size;
+            if (stream.Position + size * count >= stream.Length)
+            {
+                output = default;
+                return false;
+            }
+            
+            var buffer = new Span<byte>(new byte[size * count]);
+            if (buffer.Length != stream.Read(buffer))
+            {
+                output = default;
+                return false;
+            }
+            
+            output = buffer.ToStructs<T>();
             return true;
         }
     }
