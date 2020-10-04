2582ee1c341e4a44c7b86803a01f4ca96320061b
Matodor <matodor-zlo@yandex.ru>
2020-10-05 02:22:36 +0400

    MapItem types

---
+++ TeeSharp.Map/src/MapItemType.cs
@@ -0,0 +1,13 @@
+namespace TeeSharp.Map
+{
+    public enum MapItemType : int
+    {
+        Version = 0,
+        Info,
+        Image,
+        Envelope,
+        Group,
+        Layer,
+        EnvelopePoints
+    }
+}
