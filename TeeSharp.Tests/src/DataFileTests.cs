baf4973a53798a863128d67266971d1ea5dc0ade
Matodor <matodor-zlo@yandex.ru>
2020-10-05 10:16:43 +0400

    update DataFileTests

--- TeeSharp.Tests/src/DataFileTests.cs
+++ TeeSharp.Tests/src/DataFileTests.cs
@@ -14,13 +14,22 @@ namespace TeeSharp.Tests
         {
             var data = new byte[]
             {
-                68, 65, 84, 65, 4, 0, 0, 0, 156, 125, 3, 0, 252, 12, 0, 0, 7, 0, 0, 0, 39, 0, 0, 0, 36, 0, 0, 0, 216,
-                10, 0, 0, 160, 112, 3, 0, 0
+                68, 65, 84, 65, 4, 0, 0, 0, 215, 93, 4, 0, 212, 11, 0, 0, 6, 0, 0, 0, 34, 0, 0, 0, 41, 0, 0, 0, 168, 9,
+                0, 0, 3, 82, 4, 0, 0
             };
 
             var header = data.AsSpan().ToStruct<DataFileHeader>();
             Assert.True(header.IsValidSignature);
             Assert.True(header.IsValidVersion);
+            
+            Assert.AreEqual(header.DataSize, 283139);
+            Assert.AreEqual(header.ItemSize, 2472);
+            Assert.AreEqual(header.RawDataBlocks, 41);
+            Assert.AreEqual(header.ItemTypesCount, 6);
+            Assert.AreEqual(header.ItemsCount, 34);
+            Assert.AreEqual(header.Size, 286167);
+            Assert.AreEqual(header.SwapLength, 3028);
+            Assert.AreEqual(header.Version, 4);
         }
     }
 }
