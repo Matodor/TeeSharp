1f1c8f316a5a7f17232f6fd56abf004e4c0e5409
Matodor <matodor-zlo@yandex.ru>
2020-10-12 22:52:18 +0400

    MapItemImage

---
+++ TeeSharp.Map/src/MapItemImage.cs
@@ -0,0 +1,14 @@
+using System.Runtime.InteropServices;
+
+namespace TeeSharp.Map
+{
+    public struct MapItemImage
+    {
+        public int Version;
+        public int Width;
+        public int Height;
+        public int External;
+        public int DataIndexName;
+        public int DateIndexImage;
+    }
+}
