db458fa038f2390409019404af3544abd8442264
Matodor <matodor-zlo@yandex.ru>
2020-10-12 23:13:54 +0400

    update tests

--- TeeSharp.Tests/src/DataFileTests.cs
+++ TeeSharp.Tests/src/DataFileTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Text;
 using NUnit.Framework;
 using TeeSharp.Core.Extensions;
 using TeeSharp.Map;
@@ -12,18 +13,20 @@ namespace TeeSharp.Tests
         [Test]
         public void DeserializeDatafileHeaderTest()
         {
-            var data = new byte[]
+            // map header for "Kobra 4"
+            var data = (Span<byte>) new byte[]
             {
                 68, 65, 84, 65, 4, 0, 0, 0, 215, 93, 4, 0, 212, 11, 0, 0, 6, 0, 0, 0, 34, 0, 0, 0, 41, 0, 0, 0, 168, 9,
-                0, 0, 3, 82, 4, 0, 0
+                0, 0, 3, 82, 4, 0
             };
 
-            var header = data.AsSpan().ToStruct<DataFileHeader>();
+            var header = data.Deserialize<DataFileHeader>();
+                
             Assert.True(header.IsValidSignature);
             Assert.True(header.IsValidVersion);
             
-            Assert.AreEqual(header.DataSize, 283139);
-            Assert.AreEqual(header.ItemSize, 2472);
+            Assert.AreEqual(header.RawDataBlocksSize, 283139);
+            Assert.AreEqual(header.ItemsSize, 2472);
             Assert.AreEqual(header.RawDataBlocks, 41);
             Assert.AreEqual(header.ItemTypesCount, 6);
             Assert.AreEqual(header.ItemsCount, 34);
--- TeeSharp.Tests/src/SizeofTests.cs
+++ TeeSharp.Tests/src/SizeofTests.cs
@@ -1,21 +1,16 @@
 using NUnit.Framework;
 using TeeSharp.Core;
+using TeeSharp.Map;
 using TeeSharp.MasterServer;
 
 namespace TeeSharp.Tests
 {
     public class SizeofTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void CheckSize()
         {
-            Assert.Fail();
-            // Assert.AreEqual(18, TypeHelper<ServerEndpoint>.Size);
+            Assert.AreEqual(36, TypeHelper<DataFileHeader>.Size);
         }
     }
 }
