8f6a800a8fc6325cd903994e74573183b2b74b05
Matodor <matodor-zlo@yandex.ru>
2020-10-12 22:55:11 +0400

    TypeHelper add IsArray / Size / ElementSize properties

--- TeeSharp.Core/src/TypeHelper.cs
+++ TeeSharp.Core/src/TypeHelper.cs
@@ -5,6 +5,18 @@ namespace TeeSharp.Core
 {
     public static class TypeHelper<T>
     {
-        public static readonly int Size = Unsafe.SizeOf<T>();
+        // ReSharper disable StaticMemberInGenericType
+        public static readonly int Size;
+        public static readonly int ElementSize;
+        public static readonly bool IsArray;
+        // ReSharper restore StaticMemberInGenericType
+
+        static TypeHelper()
+        {
+            var type = typeof(T);
+            IsArray = type.IsArray;
+            Size = Marshal.SizeOf(type);
+            ElementSize = type.IsArray ? Marshal.SizeOf(type.GetElementType()) : Size;
+        }
     }
 }
