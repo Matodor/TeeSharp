aafd61b27611490f57b7bdd59ae36f77a4878c43
Matodor <matodor-zlo@yandex.ru>
2020-10-12 23:45:08 +0400

    update map parser example

--- Examples/Examples.Map/src/Program.cs
+++ Examples/Examples.Map/src/Program.cs
@@ -17,11 +17,85 @@ namespace Examples.Map
                 }
                 else
                 {
-                    var dataFile = DataFileReader.Read(stream, out var error);
+                    if (DataFileReader.Read(stream, out var error, out var dataFile))
+                    {
+                        Console.WriteLine($"Loaded, map: {MapName}");
+
+                        ShowVersion(dataFile);
+                        ShowImages(dataFile);
+                        ShowInfo(dataFile);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Loading error: {error}");
+                    }
                 }
             }
 
             Console.ReadKey();
+        }
+
+        private static void ShowInfo(DataFile dataFile)
+        {
+            if (dataFile.HasItemType((int) MapItem.Info))
+            {
+                foreach (var item in dataFile.GetItems<MapItemInfo>((int) MapItem.Info))
+                {
+                    var itemVersion = item.ItemVersion;
+                    Console.WriteLine($"MapItemInfo version: {itemVersion}");
+
+                    if (item.DataIndexAuthor > -1)
+                    {
+                        var author = dataFile.GetDataAsString(item.DataIndexAuthor);
+                        Console.WriteLine($"Map author: {author}");
+                    }
+
+                    if (item.DataIndexVersion > -1)
+                    {
+                        var version = dataFile.GetDataAsString(item.DataIndexVersion);
+                        Console.WriteLine($"Map version: {version}");
+                    }
+
+                    if (item.DataIndexCredits > -1)
+                    {
+                        var credits = dataFile.GetDataAsString(item.DataIndexCredits);
+                        Console.WriteLine($"Map credits: {credits}");
+                    }
+
+                    if (item.DataIndexLicense > -1)
+                    {
+                        var license = dataFile.GetDataAsString(item.DataIndexLicense);
+                        Console.WriteLine($"Map license: {license}");
+                    }
+                }
+                            
+                Console.WriteLine("--------------------------------------");
+            }
+        }
+
+        private static void ShowImages(DataFile dataFile)
+        {
+            if (dataFile.HasItemType((int) MapItem.Image))
+            {
+                foreach (var item in dataFile.GetItems<MapItemImage>((int) MapItem.Image))
+                {
+                    var imageName = dataFile.GetDataAsString(item.DataIndexName);
+                    Console.WriteLine($"Image: {imageName}");
+                }
+                            
+                Console.WriteLine("--------------------------------------");
+            }
+        }
+
+        private static void ShowVersion(DataFile dataFile)
+        {
+            if (dataFile.HasItemType((int) MapItem.Version))
+            {
+                foreach (var item in dataFile.GetItems<MapItemVersion>((int) MapItem.Version))
+                    Console.WriteLine($"Map version: {item.Version}");
+                            
+                Console.WriteLine("--------------------------------------");
+            }
         }
     }
 }
