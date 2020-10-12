88cad7edf1065cd146974af5ee5a0d5e79fd6643
Matodor <matodor-zlo@yandex.ru>
2020-10-12 22:52:27 +0400

    MapItemInfo

---
+++ TeeSharp.Map/src/MapItemInfo.cs
@@ -0,0 +1,11 @@
+namespace TeeSharp.Map
+{
+    public struct MapItemInfo
+    {
+        public int ItemVersion;
+        public int DataIndexAuthor;
+        public int DataIndexVersion;
+        public int DataIndexCredits;
+        public int DataIndexLicense;
+    }
+}
