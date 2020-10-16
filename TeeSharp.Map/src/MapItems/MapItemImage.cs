b02216105fe382452cb972f81221fc2b334e4044
Matodor <matodor-zlo@yandex.ru>
2020-10-16 23:30:10 +0400

    MapItemImage make External prop boolean

--- TeeSharp.Map/src/MapItems/MapItemImage.cs
+++ TeeSharp.Map/src/MapItems/MapItemImage.cs
@@ -6,10 +6,16 @@ namespace TeeSharp.Map
     {
         public const int CurrentVersion = 2;
         
+        public bool IsExternal
+        {
+            get => _external == 1;
+            set => _external = value ? 1 : 0;
+        }
+        
         public int ItemVersion;
         public int Width;
         public int Height;
-        public int External;
+        private int _external;
         public int DataIndexName;
         public int DateIndexImage;
         public int Format;
