10ad09f717fc6c078e8c6b243beeb7c945094c09
Matodor <matodor-zlo@yandex.ru>
2020-10-16 16:00:33 +0400

    ServerEndpoint.Serialize

--- TeeSharp.MasterServer/src/ServerEndpoint.cs
+++ TeeSharp.MasterServer/src/ServerEndpoint.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using TeeSharp.Network;
@@ -13,7 +14,7 @@ namespace TeeSharp.MasterServer
          */
         public const int SizeOfAddr = 18;
         
-        public static IPEndPoint Get(Span<byte> data)
+        public static IPEndPoint Deserialize(Span<byte> data)
         {
             if (data.Length < SizeOfAddr)
                 throw new ArgumentException(nameof(data));
@@ -24,16 +25,32 @@ namespace TeeSharp.MasterServer
             return new IPEndPoint(new IPAddress(isIpV4 ? data.Slice(12, 4) : data), port);
         }
         
-        public static IPEndPoint[] GetArray(Span<byte> data)
+        public static IPEndPoint[] DeserializeMultiple(Span<byte> data)
         {
             if (data.Length < SizeOfAddr || data.Length % SizeOfAddr != 0)
                 throw new ArgumentException(nameof(data));
             
             var array = new IPEndPoint[data.Length / SizeOfAddr];
             for (var i = 0; i < array.Length; i++)
-                array[i] = Get(data.Slice(i * SizeOfAddr));
+                array[i] = Deserialize(data.Slice(i * SizeOfAddr));
 
             return array;
+        }
+
+        public static Span<byte> Serialize(IPEndPoint endPoint)
+        {
+            var buffer = new Span<byte>(new byte[SizeOfAddr]);
+            NetworkConstants.IpV4Mapping.AsSpan().CopyTo(buffer);
+            
+            if (endPoint.Address.AddressFamily == AddressFamily.InterNetwork)
+                endPoint.Address.TryWriteBytes(buffer.Slice(12, 4), out var _);
+            else
+                endPoint.Address.TryWriteBytes(buffer, out var _);
+            
+            buffer[16] = (byte) ((endPoint.Port >> 8) & 0b_11111111);
+            buffer[17] = (byte) (endPoint.Port & 0b_11111111);
+
+            return buffer;
         }
     }
 }
