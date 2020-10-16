17a5ed37fdfd22d272e206d53a7b85a3182f9ea7
Matodor <matodor-zlo@yandex.ru>
2020-10-16 16:00:50 +0400

    MapItemImage add format property

--- TeeSharp.Map/src/MapItems/MapItemImage.cs
+++ TeeSharp.Map/src/MapItems/MapItemImage.cs
@@ -4,11 +4,14 @@ namespace TeeSharp.Map
 {
     public struct MapItemImage : IDataFileItem
     {
-        public int Version;
+        public const int CurrentVersion = 2;
+        
+        public int ItemVersion;
         public int Width;
         public int Height;
         public int External;
         public int DataIndexName;
         public int DateIndexImage;
+        public int Format;
     }
 }
