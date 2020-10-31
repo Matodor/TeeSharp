027ad8c30726d6bd0cdd93cbb83b922073137bcf
Matodor <matodor-zlo@yandex.ru>
2020-10-31 23:05:43 +0400

    NetworkConstants added: SecurityTokenUnknown,SecurityTokenUnsupported, MaxPayload, PacketHeaderSize, PacketConnLessDataOffset

--- TeeSharp.Network/src/NetworkConstants.cs
+++ TeeSharp.Network/src/NetworkConstants.cs
@@ -3,7 +3,13 @@ namespace TeeSharp.Network
     public static class NetworkConstants
     {
         public static readonly byte[] IpV4Mapping = new byte[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 255, 255};
+        public static readonly SecurityToken SecurityTokenUnknown = -1;
+        public static readonly SecurityToken SecurityTokenUnsupported = 0;
         
+        public const int MaxPayload = MaxPacketSize - PacketConnLessDataOffset;
         public const int MaxPacketSize = 1400;
+        public const int PacketHeaderSize = 3;
+        public const int PacketConnLessDataOffset = 6;
+
     }
 }
