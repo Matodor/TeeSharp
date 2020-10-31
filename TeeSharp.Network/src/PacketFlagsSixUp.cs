ab247c249777fcb5e7e63d726ec3b2b45aa86164
Matodor <matodor-zlo@yandex.ru>
2020-10-31 22:57:07 +0400

    PacketFlagsSixUp enum

---
+++ TeeSharp.Network/src/PacketFlagsSixUp.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TeeSharp.Network
+{
+    [Flags]
+    public enum PacketFlagsSixUp
+    {
+        None           = 0,
+        Control        = 1 << 0,
+        Resend         = 1 << 1,
+        Compression    = 1 << 2,
+        ConnectionLess = 1 << 3,
+    }
+}
