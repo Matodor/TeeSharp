58feabe13d54be7d56e28d8b7e726996f5b139e7
Matodor <matodor-zlo@yandex.ru>
2021-02-25 19:51:37 +0400

    implement base ChunkFactory

---
+++ TeeSharp.Network/src/ChunkFactory.cs
@@ -0,0 +1,41 @@
+using System.Net;
+
+namespace TeeSharp.Network
+{
+    public class ChunkFactory : BaseChunkFactory
+    {
+        public override void Init()
+        {
+        }
+
+        public override void Reset()
+        {
+            HasError = true;
+            EndPoint = null;
+            ClientId = -1;
+            Connection = null;
+            ProcessedChunks = 0;
+        }
+
+        public override void Start(IPEndPoint endPoint, BaseNetworkConnection connection, int clientId)
+        {
+            HasError = false;
+            EndPoint = endPoint;
+            ClientId = clientId;
+            Connection = connection;
+            ProcessedChunks = 0;
+        }
+
+        public override bool TryGet(out NetworkMessage netMsg)
+        {
+            if (HasError)
+            {
+                netMsg = null;
+                return false;
+            }
+            
+            netMsg = null;
+            return false;
+        }
+    }
+}
