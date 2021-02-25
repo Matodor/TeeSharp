a176c2cae09dbe3937ee8ec10f041ae6ff41cf03
Matodor <matodor-zlo@yandex.ru>
2021-02-25 19:53:34 +0400

    SecurityToken: add default TokenUnknown / TokenUnsupported, SecurityToken implemented IEquatable interface, SecurityToken implicit operator for byte array

--- TeeSharp.Network/src/SecurityToken.cs
+++ TeeSharp.Network/src/SecurityToken.cs
@@ -1,8 +1,13 @@
+using System;
+
 namespace TeeSharp.Network
 {
-    public struct SecurityToken
+    public readonly struct SecurityToken : IEquatable<SecurityToken>
     {
-        private int _value;
+        public static readonly SecurityToken TokenUnknown = -1;
+        public static readonly SecurityToken TokenUnsupported = 0;
+        
+        private readonly int _value;
         
         private SecurityToken(int value)
         {
@@ -12,6 +17,42 @@ namespace TeeSharp.Network
         public static implicit operator SecurityToken(int value)
         {
             return new SecurityToken(value);
+        }
+
+        public static implicit operator SecurityToken(byte[] data)
+        {
+            // ReSharper disable once ArrangeRedundantParentheses
+            return new SecurityToken(
+                (data[0]) |
+                (data[1] << 8) |
+                (data[2] << 16) |
+                (data[3] << 24)
+            );
+        }
+
+        public bool Equals(SecurityToken other)
+        {
+            return _value == other._value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is SecurityToken other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value;
+        }
+
+        public static bool operator ==(SecurityToken left, SecurityToken right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(SecurityToken left, SecurityToken right)
+        {
+            return !left.Equals(right);
         }
     }
 }
