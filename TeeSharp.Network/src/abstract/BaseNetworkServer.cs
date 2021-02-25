c1adfd9853e1b511fa458b33470713c5a6fc5376
Matodor <matodor-zlo@yandex.ru>
2021-02-25 19:52:09 +0400

    move some sources files

--- TeeSharp.Network/src/abstract/BaseNetworkServer.cs
+++ TeeSharp.Network/src/abstract/BaseNetworkServer.cs
@@ -9,15 +9,14 @@ namespace TeeSharp.Network
     {
         public Container Container { get; set; }
         
+        protected BaseChunkFactory ChunkFactory { get; set; }
         protected UdpClient Socket { get; set; }
-
+        
         public abstract void Init();
-        
         public abstract void Update();
-        
         // ReSharper disable once InconsistentNaming
         public abstract bool Open(IPEndPoint localEP);
-
-        public abstract bool Receive();
+        public abstract bool Receive(out NetworkMessage netMsg, ref SecurityToken responseToken);
+        public abstract SecurityToken GetToken(IPEndPoint endPoint);
     }
 }
