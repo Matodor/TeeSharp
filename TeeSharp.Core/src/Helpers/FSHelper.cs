6af0da69b2e3f67d9516ed7f7ea10d11ccf12ef0
Matodor <matodor-zlo@yandex.ru>
2021-02-28 02:23:33 +0400

    implement file system helper

---
+++ TeeSharp.Core/src/Helpers/FSHelper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.IO;
+
+namespace TeeSharp.Core.Helpers
+{
+    /// <summary>
+    /// File system helper
+    /// </summary>
+    public static class FSHelper
+    {
+        public static string WorkingPath(string relativePath = null)
+        {
+            return FirstOrCombine(
+                dir: AppDomain.CurrentDomain.BaseDirectory,
+                relativePath: relativePath
+            );
+        }
+
+        public static string AppDataPath(string relativePath = null)
+        {
+            return FirstOrCombine(
+                dir: Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                relativePath: relativePath
+            );
+        }
+
+        public static string FirstOrCombine(string dir, string relativePath)
+        {
+            return string.IsNullOrEmpty(relativePath)
+                ? dir
+                : Path.Combine(dir, relativePath);
+        }
+    }
+}
