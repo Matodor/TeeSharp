7330aeda7b0148d375c33c9daf9b4ca5159b0afa
Matodor <matodor-zlo@yandex.ru>
2021-03-01 04:17:53 +0400

    implement Limit extension method for strings

--- TeeSharp.Core/src/Extensions/StringExtensions.cs
+++ TeeSharp.Core/src/Extensions/StringExtensions.cs
@@ -8,5 +8,12 @@ namespace TeeSharp.Core.Extensions
         {
             return array.ToString();
         }
+        
+        public static string Limit(this string source, int maxLength)
+        {
+            return maxLength <= 0 || source.Length <= maxLength
+                ? source
+                : source.Substring(0, maxLength);
+        }
     }
 }
