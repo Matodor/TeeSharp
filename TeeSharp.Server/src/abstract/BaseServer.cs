fd6fcfd4a430061dbbb1c9eb0f346b973b4934cf
Matodor <matodor-zlo@yandex.ru>
2021-04-02 00:48:48 +0400

    BaseServer add ProcessNetworkMessage/ProcessMasterServerMessage/ProcessClientMessage abstract methods

--- TeeSharp.Server/src/abstract/BaseServer.cs
+++ TeeSharp.Server/src/abstract/BaseServer.cs
@@ -9,19 +9,26 @@ namespace TeeSharp.Server
     public abstract class BaseServer : IServiceBinder, IContainerService
     {
         public const int TickRate = 50;
-        
+
         public Container Container { get; set; }
         public TimeSpan GameTime { get; protected set; }
         public ServerState ServerState { get; protected set; }
         public abstract int Tick { get; protected set; }
-        
+
         protected BaseNetworkServer NetworkServer { get; set; }
         protected BaseConfiguration Config { get; set; }
         protected BaseStorage Storage { get; set; }
-        
+
         public abstract void Init();
         public abstract void Run();
         public abstract void Stop();
         public abstract void ConfigureServices(Container services);
+
+        protected abstract void ProcessNetworkMessage(
+            NetworkMessage msg, SecurityToken responseToken);
+        protected abstract void ProcessMasterServerMessage(
+            NetworkMessage msg, SecurityToken responseToken);
+        protected abstract void ProcessClientMessage(
+            NetworkMessage msg);
     }
 }
