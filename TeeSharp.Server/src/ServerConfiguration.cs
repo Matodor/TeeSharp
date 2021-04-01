4585f875388fa5056abd44d3414d2ebf29f98a67
Matodor <matodor-zlo@yandex.ru>
2021-04-02 00:49:31 +0400

    ServerConfiguration add ServerPort property

--- TeeSharp.Server/src/ServerConfiguration.cs
+++ TeeSharp.Server/src/ServerConfiguration.cs
@@ -1,16 +1,24 @@
 // ReSharper disable ArgumentsStyleStringLiteral
 // ReSharper disable ArgumentsStyleLiteral
 // ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable ArgumentsStyleNamedExpression
 using TeeSharp.Common.Config;
 
 namespace TeeSharp.Server
 {
     public class ServerConfiguration : Configuration
     {
-        public ConfigVariableString ServerName { get; } = new ConfigVariableString(
+        public virtual ConfigVariableString ServerName { get; } = new ConfigVariableString(
             defaultValue: "[TeeSharp] Unnamed server",
             description: "Server name",
             maxLength: 128
+        );
+        
+        public virtual ConfigVariableInteger ServerPort { get; } = new ConfigVariableInteger(
+            defaultValue: 8303,
+            description: "Server port",
+            min: ushort.MinValue,
+            max: ushort.MaxValue
         );
     }
 }
