121c8c927e25c7e77f69ba9eaa214c44e532d178
Matodor <matodor-zlo@yandex.ru>
2021-05-28 06:03:12 +0400

    ServerInfoType enum

---
+++ TeeSharp.MasterServer/src/ServerInfoType.cs
@@ -0,0 +1,12 @@
+namespace TeeSharp.MasterServer
+{
+    public enum ServerInfoType
+    {
+        None = -1,
+        Vanilla = 0,
+        Legacy64,
+        Extended,
+        ExtendedMore,
+        InGame,
+    }
+}
