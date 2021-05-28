38b879715d9315d31596ecadb96098a02b8ae812
Matodor <matodor-zlo@yandex.ru>
2021-05-28 06:07:35 +0400

    rename ServerEndpoint to MasterServerHelper

--- TeeSharp.MasterServer/src/Helpers/MasterServerHelper.cs
+++ TeeSharp.MasterServer/src/Helpers/MasterServerHelper.cs
@@ -7,14 +7,14 @@ using TeeSharp.Network;
 
 namespace TeeSharp.MasterServer
 {
-    public static class ServerEndpoint
+    public static class MasterServerHelper
     {
         /**
          * 16 bytes for IP address, 2 bytes for port
          */
         public const int SizeOfAddr = 18;
         
-        public static IPEndPoint Deserialize(Span<byte> data)
+        public static IPEndPoint EndPointDeserialize(Span<byte> data)
         {
             if (data.Length < SizeOfAddr)
                 throw new ArgumentException(nameof(data));
@@ -25,27 +25,27 @@ namespace TeeSharp.MasterServer
             return new IPEndPoint(new IPAddress(isIpV4 ? data.Slice(12, 4) : data), port);
         }
         
-        public static IPEndPoint[] DeserializeMultiple(Span<byte> data)
+        public static IPEndPoint[] EndPointDeserializeMultiple(Span<byte> data)
         {
             if (data.Length < SizeOfAddr || data.Length % SizeOfAddr != 0)
                 throw new ArgumentException(nameof(data));
             
             var array = new IPEndPoint[data.Length / SizeOfAddr];
             for (var i = 0; i < array.Length; i++)
-                array[i] = Deserialize(data.Slice(i * SizeOfAddr));
+                array[i] = EndPointDeserialize(data.Slice(i * SizeOfAddr));
 
             return array;
         }
 
-        public static Span<byte> Serialize(IPEndPoint endPoint)
+        public static Span<byte> EndPointSerialize(IPEndPoint endPoint)
         {
             var buffer = new Span<byte>(new byte[SizeOfAddr]);
             NetworkConstants.IpV4Mapping.AsSpan().CopyTo(buffer);
             
             if (endPoint.Address.AddressFamily == AddressFamily.InterNetwork)
-                endPoint.Address.TryWriteBytes(buffer.Slice(12, 4), out var _);
+                endPoint.Address.TryWriteBytes(buffer.Slice(12, 4), out _);
             else
-                endPoint.Address.TryWriteBytes(buffer, out var _);
+                endPoint.Address.TryWriteBytes(buffer, out _);
             
             buffer[16] = (byte) ((endPoint.Port >> 8) & 0b_11111111);
             buffer[17] = (byte) (endPoint.Port & 0b_11111111);
