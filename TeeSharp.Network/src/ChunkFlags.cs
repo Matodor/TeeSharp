884a24a6ac3da21db7a2a73a50ad9c6b3c3f717a
Matodor <matodor-zlo@yandex.ru>
2021-06-11 12:39:48 +0400

    rename some flags in ChunkFlagsSixUp / ChunkFlags

--- TeeSharp.Network/src/ChunkFlags.cs
+++ TeeSharp.Network/src/ChunkFlags.cs
@@ -8,7 +8,7 @@ namespace TeeSharp.Network
         None           = 0,
         Unused         = 1 << 0,
         Token          = 1 << 1,
-        Control        = 1 << 2,
+        Connection     = 1 << 2,
         ConnectionLess = 1 << 3,
         Resend         = 1 << 4,
         Compression    = 1 << 5,
--- TeeSharp.Network/src/ChunkFlagsSixUp.cs
+++ TeeSharp.Network/src/ChunkFlagsSixUp.cs
@@ -6,7 +6,7 @@ namespace TeeSharp.Network
     public enum ChunkFlagsSixUp
     {
         None           = 0,
-        Control        = 1 << 0,
+        Connection     = 1 << 0,
         Resend         = 1 << 1,
         Compression    = 1 << 2,
         ConnectionLess = 1 << 3,
