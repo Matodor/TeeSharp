81280c5e0d481be4a9e393ca20928291c5d2f330
Matodor <matodor-zlo@yandex.ru>
2021-08-20 00:48:41 +0400

    define BaseCommandsExecutor

---
+++ TeeSharp.Common/src/Commands/BaseCommandsExecutor.cs
@@ -0,0 +1,15 @@
+using TeeSharp.Common.Commands.Parsers;
+
+namespace TeeSharp.Common.Commands
+{
+    public abstract class BaseCommandsExecutor
+    {
+        public BaseCommandsDictionary Commands { get; protected set; }
+        
+        protected ICommandLineParser LineParser { get; set; }
+        protected ICommandArgumentsParser ArgumentsParser { get; set; }
+
+        public abstract void Init();
+        public abstract bool Execute(string line);
+    }
+}
