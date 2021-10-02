642d6e602e0798e5ac489f46f716ad76854e2a5e
Matodor <matodor-zlo@yandex.ru>
2021-10-03 01:32:31 +0400

    update ParameterInfo and ParameterBuilder

--- TeeSharp.Common/src/Commands/Builders/ParameterBuilder.cs
+++ TeeSharp.Common/src/Commands/Builders/ParameterBuilder.cs
@@ -1,10 +1,31 @@
 namespace TeeSharp.Common.Commands.Builders
 {
-    public class ParameterBuilder
+    public class ParameterBuilder : IParameterInfo
     {
+        public bool IsOptional { get; protected set; } = false;
+        public bool IsRemain { get; protected set; }
+        public IArgumentReader ArgumentReader { get; protected set; }
+        
         public ParameterBuilder()
         {
-            
+        }
+
+        public ParameterBuilder WithReader<T>() where T : IArgumentReader, new()
+        {
+            ArgumentReader = ReadersContainer.GetInstance<T>();
+            return this;
+        }
+        
+        public ParameterBuilder WithOptional(bool isOptional)
+        {
+            IsOptional = isOptional;
+            return this;
+        }
+        
+        public ParameterBuilder WithRemain(bool isRemain)
+        {
+            IsRemain = isRemain;
+            return this;
         }
         
         public ParameterInfo Build()
--- TeeSharp.Common/src/Commands/ParameterInfo.cs
+++ TeeSharp.Common/src/Commands/ParameterInfo.cs
@@ -2,9 +2,17 @@ using TeeSharp.Common.Commands.Builders;
 
 namespace TeeSharp.Common.Commands
 {
-    public class ParameterInfo
+    public class ParameterInfo : IParameterInfo
     {
-        public readonly bool Required;
+        public const char ParameterString = 's';
+        public const char ParameterFloat = 'f';
+        public const char ParameterInt = 'i';
+        public const char ParameterRest = 'r';
+        public const char ParameterOptional = '?';
+        
+        public bool IsOptional { get; }
+        public bool IsRemain { get; }
+        public IArgumentReader ArgumentReader { get; }
         
         internal ParameterInfo(ParameterBuilder builder)
         {
