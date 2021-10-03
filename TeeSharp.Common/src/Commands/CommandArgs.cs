9a55abb17f345a489dd1d054a49d96efbdb5d4d2
Matodor <matodor-zlo@yandex.ru>
2021-10-03 21:33:09 +0400

    CommandArgs inherits IReadOnlyList<object> / IEquatable<CommandArgs>

--- TeeSharp.Common/src/Commands/CommandArgs.cs
+++ TeeSharp.Common/src/Commands/CommandArgs.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TeeSharp.Common.Commands
 {
-    public class CommandArgs : EventArgs
+    public class CommandArgs : EventArgs, IReadOnlyList<object>, IEquatable<CommandArgs>
     {
-        public new static readonly CommandArgs Empty = new CommandArgs();
+        public new static readonly CommandArgs Empty = new CommandArgs(Array.Empty<object>());
+
+        public int Count => Arguments.Count;
+        public object this[int index] => Arguments[index];
+
+        protected readonly IReadOnlyList<object> Arguments;  
+            
+        public CommandArgs(IReadOnlyList<object> args)
+        {
+            Arguments = args;
+        }
+
+        public IEnumerator<object> GetEnumerator()
+        {
+            return Arguments.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public bool Equals(CommandArgs other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Arguments.SequenceEqual(other.Arguments);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((CommandArgs) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Arguments != null ? Arguments.GetHashCode() : 0);
+        }
     }
 }
