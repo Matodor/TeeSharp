bfbf3017aef9892972de5b54240b39f69201e8b9
Matodor <matodor-zlo@yandex.ru>
2022-04-02 23:47:26 +0400

    fix(commands): init context property from constructor in CommandResult

--- TeeSharp.Commands/src/CommandResult.cs
+++ TeeSharp.Commands/src/CommandResult.cs
@@ -5,16 +5,18 @@ namespace TeeSharp.Commands
     public class CommandResult : ICommandResult
     {
         public CommandArgs Args { get; }
-        
         public CommandContext Context { get; }
+        public CommandResultError? Error { get; }
 
         public bool IsSuccess => !Error.HasValue;
-
-        public CommandResultError? Error { get; }
         
-        public CommandResult(CommandArgs args, CommandContext context, CommandResultError? error)
+        public CommandResult(
+            CommandArgs args, 
+            CommandContext context, 
+            CommandResultError? error)
         {
             Args = args;
+            Context = context;
             Error = error;
         }
     }
