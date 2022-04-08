acbf44966af5c37a923e229bafbff6fe86ad7e6f
Matodor <matodor-zlo@yandex.ru>
2022-04-08 23:34:45 +0400

    refactor(commands): IArgumentReader.TryParse use MaybeNullWhen attribute for value parameter

--- TeeSharp.Commands/src/IArgumentReader.cs
+++ TeeSharp.Commands/src/IArgumentReader.cs
@@ -1,8 +1,9 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TeeSharp.Commands;
 
 public interface IArgumentReader
 {
-    public bool TryRead(ReadOnlySpan<char> arg, out object value);
+    public bool TryRead(ReadOnlySpan<char> arg, [MaybeNullWhen(false)] out object value);
 }
