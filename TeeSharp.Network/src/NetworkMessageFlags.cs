965aea11dfac6ff853376cc2147b893d54618cc7
Matodor <matodor-zlo@yandex.ru>
2022-06-24 22:28:41 +0400

    update(network): NetworkMessageHeader implement Unpack method

--- TeeSharp.Network/src/NetworkMessageFlags.cs
+++ TeeSharp.Network/src/NetworkMessageFlags.cs
@@ -6,7 +6,7 @@ namespace TeeSharp.Network;
 public enum NetworkMessageFlags
 {
     None           = 0,
-    // Vital          = 1 << 0,
+    Vital          = 1 << 0,
     ConnectionLess = 1 << 1,
     // Flush          = 1 << 2,
 }
--- TeeSharp.Network/src/NetworkMessageHeader.cs
+++ TeeSharp.Network/src/NetworkMessageHeader.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TeeSharp.Network;
 
 public class NetworkMessageHeader
@@ -11,5 +13,18 @@ public class NetworkMessageHeader
         Flags = NetworkMessageFlags.None;
         Size = 0;
         Sequence = 0;
+    }
+
+    public Span<byte> Unpack(Span<byte> data)
+    {
+        Flags = (NetworkMessageFlags)(data[0] >> 6 & 3);
+        Size = (data[0] & 0b_0011_1111) << 4 | data[1] & 0b0000_1111;
+        Sequence = -1;
+
+        if (!Flags.HasFlag(NetworkMessageFlags.Vital))
+            return data.Slice(2);
+
+        Sequence = (data[1] & -0b_0001_0000) << 2 | data[2];
+        return data.Slice(3);
     }
 }
