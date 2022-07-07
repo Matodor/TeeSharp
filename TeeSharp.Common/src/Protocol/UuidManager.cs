96c402a0b1093478475f0e98b505766fe44115b2
Matodor <matodor-zlo@yandex.ru>
2022-07-07 20:51:17 +0400

    update(common): add ddnet uuid in UuidManager

--- TeeSharp.Common/src/Protocol/UuidManager.cs
+++ TeeSharp.Common/src/Protocol/UuidManager.cs
@@ -8,11 +8,23 @@ namespace TeeSharp.Common.Protocol;
 
 public static class UuidManager
 {
-    // public const int UuidOffset = 65536;
-
     public static class Common
     {
         public static readonly Uuid TeeworldsNamespace = Uuid.Parse("e05ddaaa-c4e6-4cfb-b642-5d48e80c0029");
+    }
+
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public static class DDNet
+    {
+        public static readonly Uuid RconType = CalculateUuid("rcon-type@ddnet.tw");
+        public static readonly Uuid MapDetails = CalculateUuid("map-details@ddnet.tw");
+        public static readonly Uuid Capabilities = CalculateUuid("capabilities@ddnet.tw");
+        public static readonly Uuid ClientVersion = CalculateUuid("clientver@ddnet.tw");
+        public static readonly Uuid Ping = CalculateUuid("ping@ddnet.tw");
+        public static readonly Uuid Pong = CalculateUuid("pong@ddnet.tw");
+        public static readonly Uuid ChecksumRequest = CalculateUuid("checksum-request@ddnet.tw");
+        public static readonly Uuid ChecksumResponse = CalculateUuid("checksum-response@ddnet.tw");
+        public static readonly Uuid ChecksumError = CalculateUuid("checksum-error@ddnet.tw");
     }
 
     [SuppressMessage("ReSharper", "PossiblyImpureMethodCallOnReadonlyVariable")]
