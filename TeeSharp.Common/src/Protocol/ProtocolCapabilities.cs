efc4018ed35338048ca36cf7b2825e6c066dda09
Matodor <matodor-zlo@yandex.ru>
2022-07-31 00:03:14 +0400

    update(common): update ProtocolCapabilities enum

--- TeeSharp.Common/src/Protocol/ProtocolCapabilities.cs
+++ TeeSharp.Common/src/Protocol/ProtocolCapabilities.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 
 namespace TeeSharp.Common.Protocol;
 
+[Flags]
 public enum ProtocolCapabilities
 {
+    CurrentVersion = 5,
+
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     DDNet = 1 << 0,
     ChatTimeoutCode = 1 << 1,
