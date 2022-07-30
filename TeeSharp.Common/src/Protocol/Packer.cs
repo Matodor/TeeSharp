55ad13a0f63f68e310e6844f279dc5e3086d1798
Matodor <matodor-zlo@yandex.ru>
2022-07-31 00:02:50 +0400

    feat(common): implemented message Packer

--- TeeSharp.Common/src/Protocol/Packer.cs
+++ TeeSharp.Common/src/Protocol/Packer.cs
@@ -1,6 +1,76 @@
+using System;
+using System.Runtime.CompilerServices;
+using TeeSharp.Core;
+using Uuids;
+
 namespace TeeSharp.Common.Protocol;
 
-public class Packer
+public ref struct Packer
 {
-    
+    public const int BufferSize = 2048;
+
+    public bool HasError { get; private set; }
+
+    private readonly Span<byte> _buffer;
+    private int _bufferIndex;
+
+    public Packer()
+    {
+        HasError = false;
+
+        _buffer = new byte[BufferSize];
+        _bufferIndex = 0;
+    }
+
+    public Packer(ProtocolMessage msgId, bool isSystem)
+    {
+        HasError = false;
+
+        _buffer = new byte[BufferSize];
+        _bufferIndex = 0;
+
+        AddInt((int) msgId << 1 | (isSystem ? 1 : 0));
+    }
+
+    public Packer(Uuid msgUuid, bool isSystem)
+    {
+        HasError = false;
+
+        _buffer = new byte[BufferSize];
+        _bufferIndex = 0;
+
+        AddInt(isSystem ? 1 : 0);
+        AddUuid(msgUuid);
+    }
+
+    public void AddInt(int value)
+    {
+        if (!HasError && CompressionableInt.TryPack(_buffer, value, ref _bufferIndex))
+            return;
+
+        HasError = true;
+    }
+
+    public void AddUuid(Uuid uuid)
+    {
+        if (uuid.TryWriteBytes(_buffer.Slice(_bufferIndex)))
+        {
+            _bufferIndex += Unsafe.SizeOf<Uuid>();
+            return;
+        }
+
+        HasError = true;
+    }
+
+    public void AddRaw(Span<byte> data)
+    {
+        if (!HasError && _bufferIndex + data.Length <= _buffer.Length)
+        {
+            data.CopyTo(_buffer.Slice(_bufferIndex));
+            _bufferIndex += data.Length;
+            return;
+        }
+
+        HasError = true;
+    }
 }
