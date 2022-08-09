97b59c01182ea30dfe47561305f3c42a3fa894a2
Matodor <matodor-zlo@yandex.ru>
2022-08-09 23:42:42 +0400

    update(network): NetworkMessageHeader implement Pack method

--- TeeSharp.Network/src/NetworkMessageHeader.cs
+++ TeeSharp.Network/src/NetworkMessageHeader.cs
@@ -15,16 +15,37 @@ public class NetworkMessageHeader
         Sequence = 0;
     }
 
+    public NetworkMessageHeader(NetworkMessageFlags flags, int size, int sequence)
+    {
+        Flags = flags;
+        Size = size;
+        Sequence = sequence;
+    }
+
     public Span<byte> Unpack(Span<byte> data)
     {
-        Flags = (NetworkMessageFlags)(data[0] >> 6 & 3);
-        Size = (data[0] & 0b_0011_1111) << 4 | data[1] & 0b0000_1111;
+        Flags = (NetworkMessageFlags)(data[0] >> 6 & 0b_0000_0011);
+        Size = (data[0] & 0b_0011_1111) << 4 | data[1] & 0b_0000_1111;
         Sequence = -1;
 
         if (!Flags.HasFlag(NetworkMessageFlags.Vital))
             return data.Slice(2);
 
         Sequence = (data[1] & -0b_0001_0000) << 2 | data[2];
+        return data.Slice(3);
+    }
+
+    public Span<byte> Pack(Span<byte> data)
+    {
+        data[0] = (byte)(((int) Flags & 3) << 6 | Size >> 4 & 0b_0011_1111);
+        data[1] = (byte)(Size & 0b_0000_1111);
+
+        if (!Flags.HasFlag(NetworkMessageFlags.Vital))
+            return data.Slice(2);
+
+        data[1] |= (byte) (Sequence >> 2 & -0b_0001_0000);
+        data[2] = (byte)(Sequence & 0b_1111_1111);
+
         return data.Slice(3);
     }
 }
