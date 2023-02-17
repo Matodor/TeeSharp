b0acbecca8c7dda7a219b24a98e868fb03d41398
Matodor <matodor-zlo@yandex.ru>
2023-02-17 22:55:17 +0400

    update(masterserver): implemented MasterServerProtocolTypeExtensions

--- TeeSharp.MasterServer/src/MasterServerProtocolType.cs
+++ TeeSharp.MasterServer/src/MasterServerProtocolType.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TeeSharp.MasterServer;
 
 public enum MasterServerProtocolType
@@ -10,8 +12,27 @@ public enum MasterServerProtocolType
 
 public static class MasterServerProtocolTypeExtensions
 {
-    // public static string asd(this MasterServerProtocolType type)
-    // {
-    //
-    // }
+    public static string ToScheme(this MasterServerProtocolType type)
+    {
+        return type switch
+        {
+            MasterServerProtocolType.SixIPv6 => "tw-0.6+udp://",
+            MasterServerProtocolType.SixIPv4 => "tw-0.6+udp://",
+            MasterServerProtocolType.SixupIPv6 => "tw-0.7+udp://",
+            MasterServerProtocolType.SixupIPv4 => "tw-0.7+udp://",
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+
+    public static string ToStringRepresentation(this MasterServerProtocolType type)
+    {
+        return type switch
+        {
+            MasterServerProtocolType.SixIPv6 => "tw0.6/ipv6",
+            MasterServerProtocolType.SixIPv4 => "tw0.6/ipv4",
+            MasterServerProtocolType.SixupIPv6 => "tw0.7/ipv6",
+            MasterServerProtocolType.SixupIPv4 => "tw0.7/ipv4",
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
 }
