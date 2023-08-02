adad65535e9950b93095009935f0255b697089e6
Matodor <matodor-zlo@yandex.ru>
2023-08-02 22:10:09 +0400

    update(masterserver): added MasterServerInteractorHeaders class

---
+++ TeeSharp.MasterServer/src/MasterServerInteractorHeaders.cs
@@ -0,0 +1,10 @@
+namespace TeeSharp.MasterServer;
+
+public static class MasterServerInteractorHeaders
+{
+    public const string Secret = "Secret";
+    public const string ChallengeSecret = "Challenge-Secret";
+    public const string ChallengeToken = "Challenge-Token";
+    public const string InfoSerial = "Info-Serial";
+    public const string Address = "Address";
+}
